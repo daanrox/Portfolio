8921f61453f011920977f7a9b1823b74decf7f39
Daanrox <daanielrox@hotmail.com>
2023-02-22 09:44:25 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".digitalhouse",
+  `Trabalhei como criador de filtros para o Instagram. Nessa função, 
+  fui responsável por desenvolver e implementar conceitos de filtros para imagens e vídeos que fossem criativos, 
+  atraentes e relevantes para o público do Instagram. Utilizei habilidades avançadas em programas de design, bem como 
+  conhecimentos técnicos em linguagens de programação, como Spark AR e JavaScript. Para garantir que os filtros
+  atendessem aos objetivos do cliente e melhorassem a experiência do usuário`,
+  "Meta Spark Creator",
+  "Meta",
+  "Nov 2018 - Atualmente (4 anos)"
+);
+
+hoverChangeExperience(
+  ".zuplae",
+  `Trabalho gerenciando uma equipe de editores de fotografia, garantindo a entrega de imagens de alta 
+  qualidade dentro dos prazos estabelecidos. Desenvolvendo e implementando processos eficientes de edição de fotografia, 
+  aumentando a produtividade e eficiência da equipe. Colaborando com outros departamentos para garantir que as imagens 
+  atendam aos requisitos de qualidade e de marca da empresa. `,
+  "Coordenador de Edição",
+  "TakePic",
+  "Mar 2023 - Atualmente (1 mês)"
+);
+
+hoverChangeExperience(
+  ".codigofontetv",
+  `Trabalhei como Social Media e Designer no Código Fonte TV, 
+  onde teve como foco as criações de conteúdos sobre programação 
+  para a comunidade dev através das redes sociais.`,
+  "Social Media e Designer",
+  "Código Fonte TV",
+  "Jun 2021 - Jan 2022 (8 meses)"
+);
+
+hoverChangeExperience(
+  ".contweb",
+  `Trabalhei como Técnico de Transmissão em uma empresa de mídia, onde fui responsável por garantir que o 
+  equipamento de transmissão ao vivo fosse configurado e operado de forma adequada, permitindo que os eventos fossem transmitidos 
+  com qualidade e sem interrupções, a fim de proporcionar uma experiência positiva aos espectadores.`,
+  "Técnico de Transmissão",
+  "Painhas",
+  "Ago 2022 - Outubro 2022 (3 meses)"
+);
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".radix",
+  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+);
+hoverChangeDescription(
+  ".cypress",
+  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
