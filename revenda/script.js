69301f356d7709987450353b63e6e7b65b04331e
Daanrox <daanielrox@hotmail.com>
2023-08-03 18:01:01 -0300

    add revenda page

---
+++ revenda/script.js
@@ -0,0 +1,11 @@
+function redirectToRevendaPage() {
+    window.location.href = 'https://daanrox.com/';
+  }
+  
+  const sairButton = document.querySelector('.header__exit');
+  sairButton.addEventListener('click', redirectToRevendaPage);
+
+
+
+
+  
