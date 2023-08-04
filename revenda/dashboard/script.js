795b33ac1f38ed6c80375d43d7d1e05d88520832
Daanrox <daanielrox@hotmail.com>
2023-08-04 20:08:43 -0300

    update functions

--- revenda/dashboard/script.js
+++ revenda/dashboard/script.js
@@ -1,106 +1,189 @@
-// script_dashboard.js
+// script.js
 import { services } from './database.js';
-// Função para verificar se o token é válido
+
 function verifyToken() {
-    // Obter o token do localStorage
     const authToken = localStorage.getItem('authToken');
-  
+
     if (authToken) {
-      // Configurar as opções da requisição GET
-      const requestOptions = {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${authToken}`
-        }
-      };
-  
-      // Fazer a requisição GET para a rota de verificação do token
-      fetch('https://x8ki-letl-twmt.n7.xano.io/api:QeSM43R0/auth/me', requestOptions)
-        .then(response => response.json())
-        .then(data => {
-          // Verificar se o objeto de resposta contém a chave "balance"
-          if (data.balance !== undefined) {
-            // Atualizar o elemento <span> com o saldo formatado em moeda BRL
-            const balanceDisplay = document.querySelector('.balance__display span');
-            const formattedBalance = formatCurrencyBRL(data.balance);
-            balanceDisplay.textContent = formattedBalance;
-          } if(data.name !== undefined) {
-            const nameUser = document.querySelector('.name__user');
-            nameUser.textContent = data.name;
-           } else {
-            console.error('Erro: Saldo não encontrado na resposta da requisição.');
-          }
-        })
-        .catch(error => {
-          console.error('Erro na requisição:', error);
-        });
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${authToken}`
+            }
+        };
+
+        fetch('https://x8ki-letl-twmt.n7.xano.io/api:QeSM43R0/auth/me', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                if (data.balance !== undefined) {
+                    const balanceDisplay = document.querySelector('.balance__display span');
+                    const formattedBalance = formatCurrencyBRL(data.balance);
+                    balanceDisplay.textContent = formattedBalance;
+                }
+                if (data.name !== undefined) {
+                    const nameUser = document.querySelector('.name__user');
+                    nameUser.textContent = data.name;
+                } else {
+                    console.error('Erro: Saldo não encontrado na resposta da requisição.');
+                }
+
+                if (data.id !== undefined) {
+                    const userId = data.id;
+                    const addOrderButton = document.getElementById('addOrder');
+                    addOrderButton.addEventListener('click', () => addOrder(userId));
+                }
+            })
+            .catch(error => {
+                console.error('Erro na requisição:', error);
+            });
     } else {
-      // Não há token, redirecionar para a página de login
-      redirectToLoginPage();
+        redirectToLoginPage();
     }
-  }
-  
-  // Função para formatar o valor numérico em moeda BRL
-  function formatCurrencyBRL(value) {
+}
+
+function formatCurrencyBRL(value) {
     return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
-  }
-  
-  // Função para redirecionar o usuário para a página de login
-  function redirectToLoginPage() {
+}
+
+function redirectToLoginPage() {
     window.location.href = 'https://daanrox.com/revenda';
-  }
-  
-  // Verificar o token válido quando a página é carregada
-  verifyToken();
-  
+}
 
-  // Renderizar os options no select.
-
-  function renderOptions() {
+function renderOptions() {
     const selectElement = document.getElementById('serviceSelect');
 
-    // Loop through the 'services' array and create an option for each object
     services.forEach(service => {
         const optionElement = document.createElement('option');
         optionElement.value = service.price;
         optionElement.textContent = service.name;
         optionElement.setAttribute('data-id', service.id);
-
-        // Append the option to the select element
         selectElement.appendChild(optionElement);
     });
 }
 
-// Call the function to render options when the DOM is ready
-document.addEventListener('DOMContentLoaded', renderOptions);
-
-
-
-
 function updateOrderTotal() {
-    const quantityInput = document.getElementById('quantityInput');
-    const totalSpan = document.getElementById('spanTotal');
-    const serviceSelect = document.getElementById('serviceSelect');
+  const quantityInput = document.getElementById('quantityInput');
+  const totalSpan = document.getElementById('spanTotal');
+  const serviceSelect = document.getElementById('serviceSelect');
 
-    const selectedServicePrice = parseFloat(serviceSelect.value);
-    const quantity = parseInt(quantityInput.value);
+  const selectedServicePrice = parseFloat(serviceSelect.value);
+  const quantity = parseInt(quantityInput.value);
 
-    if (!isNaN(selectedServicePrice) && !isNaN(quantity)) {
-        const total = (selectedServicePrice * quantity) / 1000; // Dividindo por 1000 para obter o valor para uma unidade
-        totalSpan.textContent = total.toFixed(2);
-    } else {
-        totalSpan.textContent = '';
-    }
+  if (!isNaN(selectedServicePrice) && !isNaN(quantity)) {
+      const total = (selectedServicePrice * quantity) / 1000;
+      totalSpan.textContent = formatCurrencyBRL(total);
+  } else {
+      totalSpan.textContent = formatCurrencyBRL(0);
+  }
 }
-// Call the function to render options when the DOM is ready
-document.addEventListener('DOMContentLoaded', renderOptions);
 
-// Add event listeners to update the order total when the quantity or service is changed
-const quantityInput = document.getElementById('quantityInput');
-quantityInput.addEventListener('input', updateOrderTotal);
+function openModal(mensagem) {
+  const modal = document.querySelector('.modal');
+  const modalContent = modal.querySelector('.modal-content');
+  const modalMessage = modal.querySelector('p');
+  modalMessage.textContent = mensagem;
+  modal.style.display = 'block';
+}
 
-const serviceSelect = document.getElementById('serviceSelect');
-serviceSelect.addEventListener('change', updateOrderTotal);
+function closeModal() {
+  const modal = document.querySelector('.modal');
+  modal.style.display = 'none';
+}
 
-// Optionally, you may want to call 'updateOrderTotal' initially to show the total even if the user hasn't typed anything yet
-updateOrderTotal();
+async function addOrder(userId) {
+  const serviceSelect = document.getElementById('serviceSelect');
+  const linkInput = document.querySelector('input[type="text"]');
+  const quantityInput = document.getElementById('quantityInput');
+
+  const idServico = serviceSelect.options[serviceSelect.selectedIndex].getAttribute('data-id');
+  const link = linkInput.value;
+  const quantidade = quantityInput.value;
+
+  const authToken = localStorage.getItem('authToken');
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  };
+
+  try {
+    const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:QeSM43R0/auth/me', requestOptions);
+    const data = await response.json();
+
+    const apiKey = data.key;
+    const saldoAtual = parseFloat(document.querySelector('.balance__display span').textContent.replace(',', '.').replace('R$ ', ''));
+    const totalPedido = parseFloat(document.getElementById('spanTotal').textContent.replace(',', '.'));
+
+    if (totalPedido > saldoAtual) {
+      openModal('Saldo insuficiente');
+    } else {
+      const queryParams = new URLSearchParams({
+        key: apiKey,
+        action: 'add',
+        service: idServico,
+        link: link,
+        quantity: quantidade
+      });
+
+      try {
+        const response = await fetch(`https://painelsmm.com.br/api/v2?${queryParams}`);
+        const data = await response.json();
+
+        if (data.success) {
+          const novoSaldo = (saldoAtual - totalPedido).toFixed(2);
+          await atualizarSaldoNoBancoDeDados(userId, novoSaldo);
+          openModal('Pedido realizado!');
+          const saldoDisplay = document.querySelector('.balance__display span');
+          saldoDisplay.textContent = formatCurrencyBRL(novoSaldo);
+        }
+      } catch (error) {
+        console.error('Erro na requisição:', error);
+      }
+    }
+  } catch (error) {
+    console.error('Erro na requisição:', error);
+  }
+}
+
+function atualizarSaldoNoBancoDeDados(userId, novoSaldo) {
+    const authToken = localStorage.getItem('authToken');
+    const opcoesRequisicao = {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user_id: userId,
+            balance: novoSaldo
+        })
+    };
+
+    fetch(`https://x8ki-letl-twmt.n7.xano.io/api:QeSM43R0/user/${userId}`, opcoesRequisicao)
+        .then(response => response.json())
+        .then(data => {
+            const saldoDisplay = document.querySelector('.balance__display span');
+            saldoDisplay.textContent = formatCurrencyBRL(novoSaldo);
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderOptions();
+
+    const quantityInput = document.getElementById('quantityInput');
+    quantityInput.addEventListener('input', updateOrderTotal);
+
+    const serviceSelect = document.getElementById('serviceSelect');
+    serviceSelect.addEventListener('change', updateOrderTotal);
+
+    const botaoFecharModal = document.querySelector('.close-modal');
+    botaoFecharModal.addEventListener('click', closeModal);
+
+    verifyToken();
+    updateOrderTotal();
+});
+
