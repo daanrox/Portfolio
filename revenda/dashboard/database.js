511b8446e1844c1b899dd231738f7ef84454afd9
Daanrox <daanielrox@hotmail.com>
2023-08-06 10:48:23 -0300

    rename description service

--- revenda/dashboard/database.js
+++ revenda/dashboard/database.js
@@ -1,27 +1,27 @@
 export const services = [
     {
         id: 617,
-        name: '𝗦𝗲𝗴𝘂𝗶𝗱𝗼𝗿𝗲𝘀 𝗕𝗿𝗮𝘀𝗶𝗹𝗲𝗶𝗿𝗼𝘀 [99%+ BR] [Q: MÉDIA] [💣20%+] [SEM REPOSIÇÃO]',
+        name: '𝗦𝗲𝗴𝘂𝗶𝗱𝗼𝗿𝗲𝘀 𝗕𝗿𝗮𝘀𝗶𝗹𝗲𝗶𝗿𝗼𝘀 [99%+ BR] [Q: MÉDIA] [💣20%+] [SEM REPOSIÇÃO] - R$ 15,00 por 1000',
         price: 15
     },
     {
         id: 628,
-        name: 'Seguidores Estrangeiros ⚡', 
+        name: 'Seguidores Estrangeiros ⚡ - R$ 8,00 por 1000', 
         price: 8
     },
     {
         id: 830,
-        name: '💖 𝗖𝘂𝗿𝘁𝗶𝗱𝗮𝘀 𝗕𝗿𝗮𝘀𝗶𝗹𝗲𝗶𝗿𝗮𝘀 [𝟡𝟡%+ 𝔹ℝ] [Q: MÉDIA]',
+        name: '💖 𝗖𝘂𝗿𝘁𝗶𝗱𝗮𝘀 𝗕𝗿𝗮𝘀𝗶𝗹𝗲𝗶𝗿𝗮𝘀 [𝟡𝟡%+ 𝔹ℝ] [Q: MÉDIA]  - R$ 8,00 por 1000',
         price: 8
     },
     {
         id: 5,
-        name:'Curtidas Estrangeiras (Baixa Qualidade) 🐢',
+        name:'Curtidas Estrangeiras (Baixa Qualidade) 🐢  - R$ 4,00 por 1000',
         price: 4
     }, 
     {
         id: 561,
-        name:'Visualizações em Video - Reels / IGTV / Video 👀 |⚡0-1h |',
+        name:'Visualizações em Video - Reels / IGTV / Video 👀 |⚡0-1h | - R$ 2,00 por 1000' ,
         price: 2
     }
 ]
